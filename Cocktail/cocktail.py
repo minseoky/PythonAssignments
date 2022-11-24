b331ebad57c26300aaecf0805737356b1c701e6e
MINSEOK CHOI <minseok184@gmail.com>
2022-11-24 19:21:20 +0900

    추가:12주차과제

--- Cocktail/cocktail.py
+++ Cocktail/cocktail.py
@@ -18,8 +18,7 @@ class TequilaBased(Cocktail):
     count = 0
 
     def __init__(self, name):
-        self.ingredients = []
-        self.name = name
+        super().__init__(name)
         TequilaBased.count += 1
 
     def show_ingredients(self):
@@ -29,13 +28,47 @@ class TequilaBased(Cocktail):
             print("%d : %s" % (index, i))
             index += 1
 
-    def show_count(self):
+    @staticmethod
+    def show_count():
         print("We have %d Tequila Type" % TequilaBased.count)
 
 
-# tequila_sunrise.show_ingredients()
+class RumBased(Cocktail):
+    count = 0
+
+    def __init__(self, name):
+        super().__init__(name)
+        RumBased.count += 1
+
+    def show_ingredients(self):
+        index = 1
+        print("+::%s's Ingredients(Rum Based)::+" % self.name)
+        for i in self.ingredients:
+            print("%d : %s" % (index, i))
+            index += 1
+
+    @staticmethod
+    def show_count():
+        print("We have %d Rum Type" % RumBased.count)
+
+
+
+#----------------------TequilaBased----------------------
 
 tequila_sunrise = TequilaBased("Tequila Sunrise")
 tequila_sunrise.ingredients = ["1 1/2 ounces tequila", "3/4 cup orange juice",
                                "3/4 ounce grenadine syrup", "Orange slice, for garnish",
                                "High-quality maraschino cherry, such as Luxardo, for garnish"]
+
+#--------------------------------------------------------
+
+#------------------------RumBased------------------------
+
+long_island_iced_tea = RumBased("Long Island Iced Tea")
+long_island_iced_tea.ingredients = ["½ fluid ounce vodka", "½ fluid ounce rum",
+                                    "½ fluid ounce gin", "½ fluid ounce tequila",
+                                    "½ fluid ounce triple sec (orange-flavored liqueur)",
+                                    "1 fluid ounce sweet and sour mix",
+                                    "1 fluid ounce cola, or to taste", "1 lemon slice"]
+
+#--------------------------------------------------------
--- Cocktail/main.py
+++ Cocktail/main.py
@@ -1,4 +1,8 @@
 import cocktail
 
-cocktail.tequila_sunrise.show_count()
+cocktail.TequilaBased.show_count()
+cocktail.RumBased.show_count()
+
+
 cocktail.tequila_sunrise.show_ingredients()
+cocktail.long_island_iced_tea.show_ingredients()
